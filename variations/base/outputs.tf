207f900215852cd40aa011d067a782b6dfe44987
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-25 21:38:08 -0500

    adf

---
+++ variations/base/outputs.tf
@@ -0,0 +1,19 @@
+output "1_COS_Bucket_Name" {
+  description = "The name of your new COS bucket. Use this in your SQL queries."
+  value       = ibm_resource_bucket.cos_bucket.bucket_name
+}
+
+output "2_Upload_Files_URL" {
+  description = "Click this link to go to your new COS bucket and upload files."
+  value       = "[https://cloud.ibm.com/objectstorage/$](https://cloud.ibm.com/objectstorage/$){ibm_resource_instance.cos_instance.guid}/buckets/${ibm_resource_bucket.cos_bucket.bucket_name}/onboarding"
+}
+
+output "3_Run_SQL_Queries_URL" {
+  description = "Click this link to open the SQL Query UI, which is pre-configured to use your bucket."
+  value       = "[https://cloud.ibm.com/sql/launch?instance_crn=$](https://cloud.ibm.com/sql/launch?instance_crn=$){ibm_resource_instance.sql_instance.crn}"
+}
+
+output "4_Sample_Query" {
+  description = "A sample query to run after you upload 'your-file.csv'."
+  value       = "SELECT * FROM cos://${ibm_resource_bucket.cos_bucket.region_location}/${ibm_resource_bucket.cos_bucket.bucket_name}/your-file.csv LIMIT 10"
+}
