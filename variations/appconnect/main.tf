20c8ae18515c8afcf5d80e4b8e2a1ed95f0c66eb
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-27 15:16:48 -0500

    asdf

--- variations/appconnect/main.tf
+++ variations/appconnect/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = ">= 1.79.0"
+      version = ">= 1.72.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -41,83 +41,14 @@ resource "ibm_resource_instance" "cos" {
   resource_group_id = local.rg_id
 }
 
-resource "ibm_resource_key" "cos_hmac" {
-  name                 = "${var.bucket_prefix}-hmac-${random_string.suffix.result}"
-  role                 = "Writer"
-  resource_instance_id = ibm_resource_instance.cos.id
-
-  parameters_json = jsonencode({
-    HMAC = true
-  })
-}
-
-resource "ibm_cos_bucket" "bucket" {
-  bucket_name          = "${var.bucket_prefix}-${random_string.suffix.result}"
-  resource_instance_id = ibm_resource_instance.cos.id
-  region_location      = var.region
-  storage_class        = "standard"
-  force_delete         = true
-
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
-  }
-}
-
 resource "ibm_code_engine_project" "proj" {
-  name              = "idl-proj-${random_string.suffix.result}"
+  name              = "idl-${random_string.suffix.result}"
   resource_group_id = local.rg_id
-}
-
-resource "ibm_code_engine_secret" "cos_secret" {
-  project_id = ibm_code_engine_project.proj.id
-  name       = "idl-cos-secret-${random_string.suffix.result}"
-  format     = "generic"
-
-  data = {
-    COS_ACCESS_KEY_ID     = ibm_resource_key.cos_hmac.credentials["cos_hmac_keys.access_key_id"]
-    COS_SECRET_ACCESS_KEY = ibm_resource_key.cos_hmac.credentials["cos_hmac_keys.secret_access_key"]
-    COS_ENDPOINT          = "https://s3.${var.region}.cloud-object-storage.appdomain.cloud"
-    COS_BUCKET            = ibm_cos_bucket.bucket.bucket_name
-  }
 }
 
 resource "ibm_code_engine_app" "idl_helper" {
   name       = "idl-helper-${random_string.suffix.result}"
   project_id = ibm_code_engine_project.proj.id
 
-  image  = var.app_image
-  cpu    = "1"
-  memory = "1G"
-  port   = 8080
-
-  scale_min_instances = 0
-  scale_max_instances = 5
-
-  run_env_variables = [
-    {
-      type = "secret"
-      name = "COS_ACCESS_KEY_ID"
-      key  = "COS_ACCESS_KEY_ID"
-      ref  = ibm_code_engine_secret.cos_secret.name
-    },
-    {
-      type = "secret"
-      name = "COS_SECRET_ACCESS_KEY"
-      key  = "COS_SECRET_ACCESS_KEY"
-      ref  = ibm_code_engine_secret.cos_secret.name
-    },
-    {
-      type  = "secret"
-      name  = "COS_ENDPOINT"
-      key   = "COS_ENDPOINT"
-      ref   = ibm_code_engine_secret.cos_secret.name
-    },
-    {
-      type  = "secret"
-      name  = "COS_BUCKET"
-      key   = "COS_BUCKET"
-      ref   = ibm_code_engine_secret.cos_secret.name
-    }
-  ]
+  image_reference = var.app_image
 }
--- variations/base/variables.tf
+++ variations/base/variables.tf
@@ -1,19 +1,22 @@
 variable "region" {
-  type    = string
-  default = "us-south"
+  type        = string
+  description = "Deployment region"
+  default     = "us-south"
 }
 
 variable "resource_group_id" {
-  type    = string
-  default = ""
+  type        = string
+  default     = ""
+  description = "Resource Group override"
 }
 
 variable "bucket_prefix" {
-  type    = string
-  default = "idl-bucket"
+  type        = string
+  default     = "idl"
+  description = "Base name for COS instance and bucket"
 }
 
 variable "app_image" {
-  type    = string
-  default = "us.icr.io/goanalog/idl-helper:latest"
+  type        = string
+  description = "Container image reference"
 }
