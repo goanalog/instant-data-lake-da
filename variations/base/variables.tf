9ce971f1a8d72adb387f54ae644889ce92f47e31
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-27 14:43:05 -0500

    asdf

--- variations/base/variables.tf
+++ variations/base/variables.tf
@@ -1,19 +1,19 @@
 variable "region" {
-  type        = string
-  default     = "us-south"
+  type    = string
+  default = "us-south"
 }
 
 variable "resource_group_id" {
-  type        = string
-  default     = ""
+  type    = string
+  default = ""
 }
 
 variable "bucket_prefix" {
-  type        = string
-  default     = "idl-bucket"
+  type    = string
+  default = "idl-bucket"
 }
 
 variable "app_image" {
-  type        = string
-  default     = "us.icr.io/goanalog/idl-helper:latest"
+  type    = string
+  default = "us.icr.io/goanalog/idl-helper:latest"
 }
